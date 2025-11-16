c22bf205473042207c46980dc644fdf62eafe21f
Ramakrishnan Variavasamy <ramakrishnanv21@gmail.com>
2025-11-16 18:23:57 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'timesheet',
+  webDir: 'www'
+};
+
+export default config;
