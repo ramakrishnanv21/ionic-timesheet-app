bfe3003a4a03cfcd6c70f1b71f7e77da3fcd464a
Ramakrishnan Variavasamy <ramakrishnanv21@gmail.com>
2025-11-29 00:56:58 +0100

    feat: Update iOS project configuration, bundle identifier, and app display name.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.timesheet.app',
-  appName: 'timesheet',
+  appId: 'com.krishnavilas.timesheetapp',
+  appName: 'TimeSheet',
   webDir: 'www'
 };
 
